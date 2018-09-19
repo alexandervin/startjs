db2bab4533b70cf3f17c8c3e9ab8f35d217140b3
Alexander_Vin <alexandr.vinitsky@gmail.com>
2018-09-19 21:48:34 +0300

    19/09

--- Listing06_03.js
+++ Listing06_03.js
@@ -1,16 +1,16 @@
-var A=[35478067943]
-var num
-for(var k=0;k<A.length;k++){
-    num=A[k]
+var A=[3,5,4,7,8,0,6,7,9,4,3];
+var num;
+for (var k=0; k < A.length; k++) {
+    num = A[k];
     try{
-        if(num==0){
-            throw "нулевое значение"
+        if(num==0) {
+            throw 'нулевое значение'
         }
-        document.write("Отличное от нуля число:" +num+ "<br>")
+        document.write("Отличное от нуля число:" +num+ "<br>");
         }
-    catch(e){
-        document.write("Внимание--"+e+"")
-        continue
+    catch(e) {
+        document.write("Внимание--" +e+ "<br>");
+        continue;
     }   
     
 }
