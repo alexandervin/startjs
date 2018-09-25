841664f8b12f016d8e25e951b3ef89876063fecd
Alexander_Vin <alexandr.vinitsky@gmail.com>
2018-09-25 20:46:50 +0300

    25.09

--- Listing06_03.js
+++ Listing06_03.js
@@ -4,7 +4,7 @@ for (var k=0; k < A.length; k++) {
     num = A[k];
     try{
         if(num==0) {
-            throw 'нулевое значение'
+            throw 'нулевое значение';
         }
         document.write("Отличное от нуля число:" +num+ "<br>");
         }
@@ -12,5 +12,13 @@ for (var k=0; k < A.length; k++) {
         document.write("Внимание--" +e+ "<br>");
         continue;
     }   
-    
+    try {
+        if(num%3==0)
+         throw new Error("Число делится на 3");
+ }  
+
+catch(e) {
+    document.write(e.message);
+}
+document.write("<hr>");
 }
